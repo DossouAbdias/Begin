87732d0775ee9a1e6e95f6ef68a1d73939b60c1b
DossouAbdias <142014387+DossouAbdias@users.noreply.github.com>
2023-08-11 15:21:44 +0100

    Update and rename README.md to README.py
    #Python #StartGame

---
+++ README.py
@@ -0,0 +1,16 @@
+"""Démarrage jeu"""
+# coding:utf-8
+jeuLance=True
+print ("")
+while jeuLance
+      choixMenu = imput(>)
+      if choixMenu == "Jouer":
+         continue
+      elif choixMenu == "quitter":
+          jeuLance= False
+         break
+      elif choixMenu == "Histoire":
+          print("Bonjour soldat!Tu es l'Agodjié tant attendu pour sauver la cite d'Abomey")
+      else :
+           print( "Désolé ! commande introuvable")
+print("À bientôt...")
